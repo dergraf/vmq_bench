3f997087bcd7f942e446d1e75ca575835a9d480d
Andre Graf <andre@dergraf.org>
2015-04-21 13:43:27 +0200

    don't plot data throughput

--- plot.r
+++ plot.r
@@ -26,12 +26,13 @@ plot1 <- bench_plot + labs(title = "Publisher and Consumer Rates (msgs/sec)") +
             geom_smooth(aes(y = nr_of_con_msgs, color = "Consumer Rate"), size=0.5) +
             geom_point(aes(y = nr_of_con_msgs, color = "Consumer Rate"), size=2.0)
 
-plot2 <- bench_plot + labs(title = "Publisher and Consumer Rates (KB/sec)") +
-            geom_smooth(aes(y = round(nr_of_pub_data / 1024), color = "Publisher Rate"), size=0.5) +
-            geom_point(aes(y = round(nr_of_pub_data / 1024), color = "Publisher Rate"), size=2.0) +
-
-            geom_smooth(aes(y = round(nr_of_con_data / 1024), color = "Consumer Rate"), size=0.5) +
-            geom_point(aes(y = round(nr_of_con_data / 1024), color = "Consumer Rate"), size=2.0)
+# TODO: measurement is still buggy..
+#plot2 <- bench_plot + labs(title = "Publisher and Consumer Rates (KB/sec)") +
+#            geom_smooth(aes(y = round(nr_of_pub_data / 1024), color = "Publisher Rate"), size=0.5) +
+#            geom_point(aes(y = round(nr_of_pub_data / 1024), color = "Publisher Rate"), size=2.0) +
+#
+#            geom_smooth(aes(y = round(nr_of_con_data / 1024), color = "Consumer Rate"), size=0.5) +
+#            geom_point(aes(y = round(nr_of_con_data / 1024), color = "Consumer Rate"), size=2.0)
 
 plot3 <- bench_plot + labs(title = "Nr of Publishers") +
             geom_line(aes(y = nr_of_pubs, color = "Nr of Publishers"))
@@ -45,13 +46,12 @@ plot5 <- bench_plot + labs(title = "Latencies in ms") +
 
 
 
-pushViewport(viewport(layout = grid.layout(5, 1, heights=c(1.5, 1.5, 0.5, 0.5, 1))))
+pushViewport(viewport(layout = grid.layout(4, 1, heights=c(1.5, 1.5, 0.5, 0.5))))
 vplayout <- function(x,y) viewport(layout.pos.row = x, layout.pos.col = y)
 
 print(plot1, vp=vplayout(1,1))
-print(plot2, vp=vplayout(2,1))
+print(plot5, vp=vplayout(2,1))
 print(plot3, vp=vplayout(3,1))
 print(plot4, vp=vplayout(4,1))
-print(plot5, vp=vplayout(5,1))
 
 dev.off()
